e37c0f9084b042611dc6e20e80a6a64dafb86775
William Falcone <wjf0003+will@gmail.com>
2018-01-25 20:12:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,15 @@
 class Dog 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+
+  def initialize(name)
+    @name = name
   end
-  
+ 
   def name
-    @this_dogs_name
+    @name
   end
-  
-  
-  
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
 end
